d6fefe3ba54fde84e224cc03c9521f2daa01de84
live <live@live-PC>
2019-11-21 21:25:44 +0800

    123

---
+++ CGB1908-2/src/main/java/com/cy/Cgb19082Application.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cgb19082Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cgb19082Application.class, args);
+	}
+
+}
---
+++ CGB1908-2/src/main/java/com/cy/common/cache/DefaultCache.java
@@ -0,0 +1,5 @@
+package com.cy.common.cache;
+
+public class DefaultCache {
+
+}
